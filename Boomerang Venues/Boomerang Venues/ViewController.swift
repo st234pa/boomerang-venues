7899b6bed84dc57041598543ac49c7b77a8e47d7
Stephanie Yoon <syoon1@mit.edu>
2020-03-26 19:36:41 -0400

    created single page app with WKWebView

--- Boomerang Venues/Boomerang Venues/ViewController.swift
+++ Boomerang Venues/Boomerang Venues/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string:"https://www.google.com")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
 
 
