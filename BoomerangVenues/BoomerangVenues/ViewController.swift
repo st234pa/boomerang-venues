1361855f0ac1eea90ca74ba601d79fd1f9e6ea6b
Stephanie Yoon <syoon1@mit.edu>
2020-03-26 19:51:32 -0400

    created single page ios app with wkwebview

---
+++ BoomerangVenues/BoomerangVenues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoomerangVenues
+//
+//  Created by STmb2 on 3/26/20.
+//  Copyright © 2020 Stephanie Yoon. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.google.com")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+    }
+
+
+}
+
