5dbcb12ca7f6171c0bf60933671e514891343702
Stephanie Yoon <syoon1@mit.edu>
2020-03-26 19:22:48 -0400

    created xcode project

---
+++ Boomerang Venues/Boomerang Venues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boomerang Venues
+//
+//  Created by STmb2 on 3/26/20.
+//  Copyright © 2020 Stephanie Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
